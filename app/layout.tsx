626285bea4ffa3c12b4f680d5a02d0711637330d
vaniuz <vaniuzmusic@gmail.com>
2026-08-14 10:12:41 -0300

    Replace favicon with VJ monogram

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
+    icon: "/favicon.svg?v=2",
+    shortcut: "/favicon.svg?v=2",
   },
 };
 
